976b5506705ec638a9e820dcd69b72eb0c567396
zamyslov@gmail.com <zamyslov@gmail.com>
2017-11-22 22:33:05 +0200

    hw3 done

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,11 +5,11 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity {
-    private final LocalDateTime dateTime;
+    private LocalDateTime dateTime;
 
-    private final String description;
+    private String description;
 
-    private final int calories;
+    private int calories;
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
@@ -19,6 +19,21 @@ public class Meal extends AbstractBaseEntity {
         super(id);
         this.dateTime = dateTime;
         this.description = description;
+        this.calories = calories;
+    }
+
+    public Meal() {
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
         this.calories = calories;
     }
 
