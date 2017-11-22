92c74f06b66d285729611818bde5c093850d7b8d
zamyslov@gmail.com <zamyslov@gmail.com>
2017-11-22 19:49:48 +0200

    repair tests

--- src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/InMemoryAdminRestControllerSpringTest.java
@@ -17,7 +17,7 @@ import java.util.Collection;
 
 import static ru.javawebinar.topjava.UserTestData.ADMIN;
 
-@ContextConfiguration("classpath:spring-app-test.xml")
+@ContextConfiguration("classpath:spring-test.xml")
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
 
