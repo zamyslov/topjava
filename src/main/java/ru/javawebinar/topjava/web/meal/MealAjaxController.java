c4ee1628c6c0a3ea810d6a88aefecebb5c8eb301
zamyslov@gmail.com <zamyslov@gmail.com>
2017-12-31 15:46:39 +0200

    HW8 #1

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealAjaxController.java
@@ -0,0 +1,38 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealWithExceed;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ajax/profile/meals")
+public class MealAjaxController extends AbstractMealController {
+
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealWithExceed> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @PostMapping
+    public void createOrUpdate(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam("dateTime") LocalDateTime dateTime,
+                               @RequestParam("description") String description,
+                               @RequestParam("calories") Integer calories) {
+
+        Meal meal = new Meal(dateTime, description, calories);
+        if (meal.isNew()) {
+            super.create(meal);
+        }
+    }
+}
